ce7f67e4e0c300607916193897ce4778bab651f9
peter <goverperson@gmail.com>
2020-12-07 10:33:28 +0800

    添加初始化函数

--- mongoq/collection.go
+++ mongoq/collection.go
@@ -3,43 +3,50 @@ package mongoq
 import (
 	"context"
 	"log"
+	"os"
+	"time"
 
-	"go.mongodb.org/mongo-driver/bson" //一种数据格式
+	//一种数据格式
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
 var client *mongo.client
 var collection *mongo.Collection
+
 //init方法初始化日志存储文件和连接数据库
-func init(){
+func init() {
 	setLog()
 }
 
-func Conn(uri string, dbName string,colName string){
-	client,_ := mongo.NewClient(options.Client().ApplyURI(uri))
-	ctx, _ := context.WithTimeout(context.Background(),10*time.Second)
+func Conn(uri string, dbName string, colName string) {
+	client, _ = mongo.NewClient(options.Client().ApplyURI(uri))
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 	err := client.Connect(ctx)
 	if err != nil {
 		log.Println("[init]", err)
 	}
 	collection = client.Database(dbName).Collection(colName)
 }
-func FineOne(){}
 
-func InsertOne(){}
+//FineOne封装了mongo的FindOne方法
+func FineOne(cname string, database string, filter interface{}) {
 
-func DeleteOne(){}
+}
 
-func UpdateOne(){}
+func InsertOne() {}
 
-func setLog(){
+func DeleteOne() {}
+
+func UpdateOne() {}
+
+func setLog() {
 	file := "./log.txt"
-	logFile, err := os.OpenFile(file,os.O_RDWR|os.O_CREATE|O_APPEND,0766)
+	logFile, err := os.OpenFile(file, os.O_RDWR|os.O_CREATE|O_APPEND, 0766)
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.SetOutput(logFile)
 	log.SetPrefix("[db]")
-	log.SetFlags(log.LstdFlags|log.Lshortfile|log.LUTC)
+	log.SetFlags(log.LstdFlags | log.Lshortfile | log.LUTC)
 }
-
